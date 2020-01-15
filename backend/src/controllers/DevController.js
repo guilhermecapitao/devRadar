90fa1bc1fdb431e837fa2cd438ae0c2b5383d702
Guilherme Capitão <guilherme.capitao@intermidia.com.br>
2020-01-15 10:02:13 -0300

    Create react app - web

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -22,6 +22,8 @@ module.exports = {
         `https://api.github.com/users/${github_username}`
       );
 
+
+
       const { name = login, avatar_url, bio } = apiResponse.data;
 
       const techsArray = parseStringAsArray(techs)
